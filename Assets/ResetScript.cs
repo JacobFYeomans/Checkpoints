3144cdc12bf5f9523b22ede8ce620a25115f2510
PKMNRenunciation <yeomansjf@gmail.com>
2022-01-26 10:07:33 -0400

    made changed according class

---
+++ Assets/ResetScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetScript : MonoBehaviour
+{
+    public CharacterController player;
+    public GameObject respawnPoint;
+    //public GameObject playerCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("Player Resetting");
+            Debug.Log(respawnPoint.transform.position);
+            Vector3 deltaMovement = respawnPoint.transform.position - player.transform.position;
+            player.detectCollisions = false;
+            player.Move(deltaMovement); //diable collision then renable afterwards
+            player.detectCollisions = true;
+        }
+        
+    }
+}
